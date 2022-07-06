7ce93cdf05fca8f22c3c25e3ab167f5c1e8511ac
Asmenys <76791232+Asmenys@users.noreply.github.com>
2022-07-06 14:39:18 +0000

    lists departures dates ordered in ascension

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,7 +2,7 @@ class FlightsController < ApplicationController
     def index
         @departure_options = Flight.all.map{|flight| [flight.departure_airport.code]}.uniq
         @arrival_options = Flight.all.map{|flight| [flight.arrival_airport.code]}.uniq
-        @departure_dates = Flight.all.map{|flight| [flight.departure_date]}.uniq
+        @departure_dates = Flight.select(:departure_date).distinct.order(departure_date: :asc).map{|flight| flight.departure_date}
         @passanger_options = Array.new(4) {|i = 0| [i+=1]}
         if params.key?("commit")
         @flights = Flight.where(departure_airport: Airport.find_by(code:params[:departure_airport]).id)
