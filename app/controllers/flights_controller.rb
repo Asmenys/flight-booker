6fd35ae3eb6387b4617a15bb46d86510ba4d277b
Asmenys <76791232+Asmenys@users.noreply.github.com>
2022-07-06 09:22:45 +0000

    Added num of passangers dropdown select option to the search form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,6 +3,7 @@ class FlightsController < ApplicationController
         @departure_options = Flight.all.map{|flight| [flight.departure_airport.code]}.uniq
         @arrival_options = Flight.all.map{|flight| [flight.arrival_airport.code]}.uniq
         @departure_dates = Flight.all.map{|flight| [flight.departure_date]}.uniq
+        @passanger_options = Array.new(4) {|i = 0| [i+=1]}
     end
 
 end
