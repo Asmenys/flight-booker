5ea5a7576638621713bc5b4883dc1769a06158ce
Asmenys <76791232+Asmenys@users.noreply.github.com>
2022-07-06 17:37:08 +0000

    Configured booking routes, created a new booking creation view

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,9 @@
 class BookingsController < ApplicationController
     def new
+        @booking = Booking.new(flight_id: params[:flight_id])
+    end
+
+    def create
+        @booking = Booking.new(flight_id: params[:flight_id])
     end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,5 @@
 class Booking < ApplicationRecord
     belongs_to :flight
     has_many :passengers, foreign_key: "booking_id"
-    def new
-        @booking = Booking.new
-    end
+    accepts_nested_attributes_for :passengers
 end
