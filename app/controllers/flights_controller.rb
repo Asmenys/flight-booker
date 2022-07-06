d4c34da4d0a852ef4113eefa17d7c37abd692a19
Asmenys <76791232+Asmenys@users.noreply.github.com>
2022-07-06 11:49:36 +0000

    form now keeps selected paramaters after redirect, displays flights that match selected parameters if they exist

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,6 +4,11 @@ class FlightsController < ApplicationController
         @arrival_options = Flight.all.map{|flight| [flight.arrival_airport.code]}.uniq
         @departure_dates = Flight.all.map{|flight| [flight.departure_date]}.uniq
         @passanger_options = Array.new(4) {|i = 0| [i+=1]}
+        if params.key?("commit")
+        @flights = Flight.where(departure_airport: Airport.find_by(code:params[:departure_airport]).id)
+        @flights = @flights.where(arrival_airport: Airport.find_by(code:params[:arrival_airport]).id)
+        @flights = @flights.where(departure_date: Date.parse(params[:departure_date]))
+        end
     end
 
 end
