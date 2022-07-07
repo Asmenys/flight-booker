2ed9c5d06b1767e801965981a09079df0bbca9ea
Asmenys <76791232+Asmenys@users.noreply.github.com>
2022-07-07 13:10:13 +0000

    created a show page for bookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_06_165105) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_07_122553) do
   create_table "airports", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -21,7 +21,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_06_165105) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "flight_id"
-    t.integer "passenger_id"
   end
 
   create_table "flights", force: :cascade do |t|
