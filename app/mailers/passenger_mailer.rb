71aa13e60e749ed07ca9bab84177597928be3aaa
Asmenys <76791232+Asmenys@users.noreply.github.com>
2022-07-15 10:32:43 +0000

    created a confirmation_email method for PassengerMailer, created text and html views for confirmation_email

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,11 @@
 class PassengerMailer < ApplicationMailer
+    default from: 'flight_booker@flights.com'
+
+    def confirmation_email(booking)
+       @user = booking.passengers.first
+       @flight = booking.flight
+       @url = "http://flight-booker/bookings/#{booking.id}"
+       @arrival_time = @flight.departure_time.advance(hours:@flight.duration)
+       mail(to: @user.email, subject: "successfull ticket order")
+    end
 end
